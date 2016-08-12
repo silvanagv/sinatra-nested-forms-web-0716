a675aeda7741df757649340896cf68f1fe13023d
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-08-12 15:58:32 -0400

    donee

--- app.rb
+++ app.rb
@@ -3,7 +3,27 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    get '/pirates/:id' do
+      @id = Pirate.find(params[:pirate][:id])
+      erb :'pirates/show'
+    end
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      # binding.pry
+      # @ship1 = Ship.new(params[:pirate][:ships][0])
+      # @ship2 = Ship.new(params[:pirate][:ships][1])
+      # redirect :'/pirates/#{pirate.id}'
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
